6419a4260bdf6738f56b782eebfda25cfc8f46da
Sakib548 <mdsakibhassan548@gmail.com>
2025-03-11 17:33:36 +0600

    First Commit

---
+++ client/src/App.jsx
@@ -0,0 +1,23 @@
+import { Route, Routes } from "react-router-dom";
+import Footer from "./components/Footer";
+import Login from "./components/Login";
+import Navbar from "./components/Navbar";
+import BuyCredit from "./pages/BuyCredit";
+import Home from "./pages/Home";
+import Result from "./pages/Result";
+
+const App = () => {
+  return (
+    <div className="px-4 sm:px-10 md:px-14 lg:px-28 min-h-screen bg-gradient-to-b from-teal-50 to-orange-50">
+      <Navbar />
+      <Login />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/result" element={<Result />} />
+        <Route path="/buy" element={<BuyCredit />} />
+      </Routes>
+      <Footer />
+    </div>
+  );
+};
+export default App;
