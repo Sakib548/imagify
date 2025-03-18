c89b9c2922313c41f8b55fd50f90a1fae9bb17fc
Sakib548 <mdsakibhassan548@gmail.com>
2025-03-18 13:11:49 +0600

    security fix cors

--- server/server.js
+++ server/server.js
@@ -9,16 +9,21 @@ const PORT = process.env.PORT || 4000;
 
 const app = express();
 
+const allowedOrigins = [
+  process.env.FRONTEND_URL,
+  "http://localhost:3000", // For local development
+];
+
 // Configure CORS options
 const corsOptions = {
-  origin: process.env.FRONTEND_URL, // Frontend origin
+  origin: allowedOrigins, // Frontend origin
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"], // Allowed methods
   allowedHeaders: ["Content-Type", "Authorization"], // Allowed headers
   credentials: true, // If using cookies/authentication
 };
 
 app.use(cors(corsOptions));
-app.options("*", cors(corsOptions)); // Handle preflight OPTIONS request
+app.options("*", cors(corsOptions)); // Preflight support
 app.use(express.json());
 
 await connectDB();
